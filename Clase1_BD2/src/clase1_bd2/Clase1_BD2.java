1846c64fb370880f68ed8edd322d0896a50de8c6
User <User@M4J6>
2020-08-05 11:12:26 -0500

    Primera Modificacion

---
+++ Clase1_BD2/src/clase1_bd2/Clase1_BD2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase1_bd2;
+
+/**
+ *
+ * @author User
+ */
+public class Clase1_BD2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x=2;
+        
+    }
+    
+}
